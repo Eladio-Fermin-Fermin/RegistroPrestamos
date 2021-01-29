4f6d036e96719bb5adc30f7c528e52cb755b7f5a
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2021-01-29 18:41:57 -0400

    Se creo la clase Prestamos en la carpeta Models

---
+++ RegistroPrestamos/Models/Prestamo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroPrestamos.Models
+{
+    public class Prestamo
+    {
+        [Key]
+        public int PrestamoId { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir la fecha")]
+        public DateTime Fecha { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir una PersonaId")]
+        public int PersonaId { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir un Concepto")]
+        public float Concepto { get; set; }
+        [Required(ErrorMessage = "Es obligatorio introducir un Monto")]
+        public float Monto { get; set; }
+        public float Balance { get; set; }
+
+
+    }
+}
