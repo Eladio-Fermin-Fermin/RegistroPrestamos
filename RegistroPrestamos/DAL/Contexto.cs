59a7ce76350d5df39bbdaca96d0bc86be2467288
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2021-01-29 19:12:43 -0400

    Se creo la clase Contexto en la carpeta DAL

---
+++ RegistroPrestamos/DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroPrestamos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroPrestamos.DAL
+{
+    public class Contexto :DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    }
+}
